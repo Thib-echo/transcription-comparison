b96b91b495f2e1042a99678951162e605fd65f29
ThibBac <thibault.bac@echoproject.fr>
2023-11-28 21:33:26 +0100

    [MAIN] - Change transcription comparison viewing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from transcription_classes.FasterWhisperTranscription import FasterWhisperTranscription
 from transcription_classes.GladiaTranscription import GladiaTranscription
 from transcription_classes.WhisperTranscription import WhisperTranscription
+from transcription_classes.StableWhisperTranscription import StableWhisperTranscription
 
 from utils import normalize_transcription, get_audio_duration, transcribe_and_time
 from html import escape
@@ -11,7 +12,7 @@ import argparse
 import time
 
 # Function to compare transcriptions
-def compare_transcriptions_to_html(transcription1, transcription2):
+def compare_transcriptions_to_html(transcription1, transcription2, color1='blue', color2='green'):
     # Normalize the transcriptions
     norm_transcription1 = normalize_transcription(transcription1).split()
     norm_transcription2 = normalize_transcription(transcription2).split()
@@ -24,13 +25,40 @@ def compare_transcriptions_to_html(transcription1, transcription2):
     html_diff = []
     for word in diff:
         if word.startswith("+ "):
-            html_diff.append(f"<span style='color:green;'>{escape(word[2:])}</span>")
+            html_diff.append(f"<span style='color:{color2};'>{escape(word[2:])}</span>")
         elif word.startswith("- "):
-            html_diff.append(f"<span style='color:red;'>{escape(word[2:])}</span>")
+            html_diff.append(f"<span style='color:{color1};'>{escape(word[2:])}</span>")
         elif word.startswith("  "):
             html_diff.append(escape(word[2:]))
 
     return ' '.join(html_diff)
+
+def generate_comparison_matrix_html(transcriptions, tool_names):
+    html_output = "<table border='1'><tr><th></th>"
+    color = {"FasterWhisper": 'red',
+             "Gladia": 'purple', 
+             "Whisper": 'green', 
+             "StableWhisper": 'blue'
+    }
+
+    # Table headers
+    for name in tool_names:
+        html_output += f"<th>{name}</th>"
+    html_output += "</tr>"
+
+    for i, (name1, transcription1) in enumerate(zip(tool_names, transcriptions)):
+        html_output += f"<tr><th>{name1}</th>"
+        for j, (name2, transcription2) in enumerate(zip(tool_names, transcriptions)):
+            if i != j:
+                # Compare and create a cell with highlighted differences
+                comparison_html = compare_transcriptions_to_html(transcription1, transcription2, color1=color[name1], color2=color[name2])
+                html_output += f"<td>{comparison_html}</td>"
+            else:
+                html_output += "<td style='background-color:lightgrey;'></td>"  # Empty cell for same-tool comparison
+        html_output += "</tr>"
+
+    html_output += "</table>"
+    return html_output
 
 def main(args):
     start_time = time.time()
@@ -47,25 +75,26 @@ def main(args):
     faster_whisper = FasterWhisperTranscription(audio_path, model_size)
     gladia = GladiaTranscription(audio_path)
     whisper = WhisperTranscription(audio_path, hf_model, model_size)
+    stable_whisper = StableWhisperTranscription(audio_path, hf_model, model_size)
 
     try:
         # Transcribe audio using the services
         transcription_fw, _ = transcribe_and_time(faster_whisper, audio_duration)
         transcription_gladia, _ = transcribe_and_time(gladia, audio_duration)
         transcription_whisper, _ = transcribe_and_time(whisper, audio_duration)
-                
-        # Compare the transcriptions and write to HTML files
-        comparisons = [
-            (transcription_fw, transcription_gladia, "fw_and_gladia"),
-            (transcription_fw, transcription_whisper, "fw_and_whisper"),
-            (transcription_whisper, transcription_gladia, "whisper_and_gladia")
-        ]
+        transcription_sw, _ = transcribe_and_time(stable_whisper, audio_duration)
 
-        Path(f"./outputs/{audio_name}").mkdir(parents=True, exist_ok=True)
-        for t1, t2, filename in comparisons:
-            result = compare_transcriptions_to_html(t1, t2)
-            with open(f"./outputs/{audio_name}/transcription_comparison_{filename}.html", "w") as file:
-                file.write(result)
+        transcriptions = [transcription_fw, transcription_gladia, transcription_whisper, transcription_sw]
+        tool_names = ["FasterWhisper", "Gladia", "Whisper", "StableWhisper"]
+
+        # Generate comparison matrix
+        comparison_matrix_html = generate_comparison_matrix_html(transcriptions, tool_names)
+
+        # Save the comparison matrix to an HTML file
+        output_filename = f"./outputs/{audio_name}/transcription_comparison_matrix.html"
+        Path(output_filename).parents[0].mkdir(parents=True, exist_ok=True)
+        with open(output_filename, "w") as file:
+            file.write(comparison_matrix_html)
 
     except Exception as e:
         print(f"Error: {e}") 
